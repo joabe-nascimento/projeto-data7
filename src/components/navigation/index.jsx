54f83513be29a162e8eaa68ea2c15bde175b2c32
Joabe Nascimento <joabefnascimento1@outlook.com>
2024-08-13 12:39:38 -0300

    ajustes da navbar, erro naa largura do modo responsivo.

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -17,35 +17,35 @@ export default function WithSubnavigation() {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
-  <Box
-  position={"fixed"}
-  top={0}
-  left={{ base: 0, md: "50%" }}  // No desktop, centraliza a navbar
-  transform={{ base: "none", md: "translateX(-50%)" }}  // Ajuste de centralização no desktop
-  width={{ base: "100%", md: "calc(100% - 48px)" }}  // Largura responsiva, fixa no desktop
-  maxWidth={{ md: "1200px" }}  // Limita a largura máxima no desktop
-  borderRadius={"30px"}
-  boxShadow={"lg"}
-  zIndex={99999999}
-  margin={4}
-  transition={"all 0.3s ease"}
->
-  <Flex
-    maxH={{ base: "20px", md: "100px" }}  // Altura máxima ajustada para modo desktop e responsivo
-    bg={useColorModeValue("rgba(255, 255, 255, 0.8)", "rgba(0, 0, 0, 0.8)")}
-    color={useColorModeValue("gray.600", "white")}
-    minH={{ base: "50px", md: "60px" }}  // Altura mínima ajustada para modo desktop e responsivo
-    py={{ base: 2, md: 4 }}  // Padding vertical ajustado para modo desktop e responsivo
-    px={{ base: 3, md: 8 }}  // Padding lateral ajustado para modo desktop e responsivo
-    borderBottom={1}
-    borderStyle={"solid"}
-    borderColor={useColorModeValue("gray.200", "gray.900")}
-    align={"center"}
-    justify={"center"}
-    backdropFilter={"blur(10px)"}
-    borderRadius={"30px"}
-    width={"100%"}  // Garante que o Flex ocupe toda a largura do Box
-  >
+    <Box
+      position={"fixed"}
+      top={0}
+      left={{ base: 0, md: "50%" }} // No desktop, centraliza a navbar
+      transform={{ base: "none", md: "translateX(-50%)" }} // Ajuste de centralização no desktop
+      width={{ base: "100%", md: "calc(100% - 48px)" }} // Largura responsiva, fixa no desktop
+      maxWidth={{ md: "1200px" }} // Limita a largura máxima no desktop
+      borderRadius={"30px"}
+      boxShadow={"lg"}
+      zIndex={99999999}
+      margin={4}
+      transition={"all 0.3s ease"}
+    >
+      <Flex
+        maxH={{ base: "none", md: "100px" }} // Altura máxima ajustada para modo desktop e responsivo
+        bg={useColorModeValue("rgba(255, 255, 255, 0.8)", "rgba(0, 0, 0, 0.8)")}
+        color={useColorModeValue("gray.600", "white")}
+        minH={{ base: "50px", md: "60px" }} // Altura mínima ajustada para modo desktop e responsivo
+        py={{ base: 2, md: 4 }} // Padding vertical ajustado para modo desktop e responsivo
+        px={{ base: 3, md: 8 }} // Padding lateral ajustado para modo desktop e responsivo
+        borderBottom={1}
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.900")}
+        align={"center"}
+        justify={"center"}
+        backdropFilter={"blur(10px)"}
+        borderRadius={"30px"}
+        width={"100%"} // Garante que o Flex ocupe toda a largura do Box
+      >
         <Flex
           flex={{ base: 1 }}
           justify={{ base: "center", md: "space-between" }}
@@ -62,7 +62,7 @@ export default function WithSubnavigation() {
               <Image
                 src={data71}
                 alt="Logo"
-                height={{ base: "120px", md: "250px" }} // Altura da logo reduzida no responsivo
+                height={{ base: "50px", md: "250px" }} // Altura da logo ajustada
                 objectFit={"contain"}
               />
             </Link>
