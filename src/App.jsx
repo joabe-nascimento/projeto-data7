b135c8085c4526d3ccf5e254824abdd98d381352
Joabe Nascimento <joabefnascimento1@outlook.com>
2024-08-15 15:27:37 -0300

    rotas funcionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import AppRoutes from "./routes/routes"; // Importa o arquivo de rotas
-import AppRoutesNotFound from "./routes/routesnotfound"; // Importa o arquivo de rotas para NotFound
+import AppRoutes from "./routes/routes"; 
+import AppRoutesNotFound from "./routes/routesnotfound";
 
 const App = () => {
   return (
