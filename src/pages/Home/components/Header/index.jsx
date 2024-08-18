614d6208336d99a05f28a44d30e3121fd5522759
joabe-nascimento <joabefnascimento1@outlook.com>
2024-08-18 13:27:46 -0300

    foi adicionado o carrosel no header

--- src/pages/Home/components/Header/index.jsx
+++ src/pages/Home/components/Header/index.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import {
   Button,
   Container,
@@ -7,39 +8,99 @@ import {
   Text,
   VStack,
   useBreakpointValue,
+  Box,
+  IconButton,
+  chakra,
 } from "@chakra-ui/react";
-import imageheaderum from "../../../../assets/imageheaderum.jpg"; // Altere o caminho conforme necessário
+import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
+
+// Importe suas imagens
+import imageheaderum from "../../../../assets/imageheaderum.jpg";
+import immageheaderdois from "../../../../assets/immageheaderdois.jpg";
+
+const images = [imageheaderum, immageheaderdois]; // Array com suas imagens
 
 const HeroSection = () => {
-  // Número de telefone e mensagem
-  const whatsappNumber = "+5575999194533"; // Substitua pelo número do WhatsApp desejado
-  const whatsappMessage = "Olá, gostaria de mais informações sobre seus serviços.";
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-  // Link para o WhatsApp com o número e a mensagem
+  const whatsappNumber = "+5575999194533";
+  const whatsappMessage =
+    "Olá, gostaria de mais informações sobre seus serviços.";
   const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(
     whatsappMessage
   )}`;
 
+  const handleNextSlide = () => {
+    setCurrentSlide((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+
+  const handlePrevSlide = () => {
+    setCurrentSlide((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
+
   return (
-    <Flex
-      w={"full"}
-      h={"100vh"}
-      position={"relative"}
-      overflow={"hidden"}
-      bgImage={`url(${imageheaderum})`} // Define a imagem de fundo
-      bgPosition={"center"}
-      bgRepeat={"no-repeat"}
-      bgSize={"cover"} // Garante que a imagem cubra toda a área
-    >
+    <Flex w={"full"} h={"100vh"} position={"relative"} overflow={"hidden"}>
+      {/* Carrossel de imagens */}
+      <Box
+        w="full"
+        h="100vh"
+        bgImage={`url(${images[currentSlide]})`}
+        bgPosition={"center"}
+        bgRepeat={"no-repeat"}
+        bgSize={"cover"}
+        transition="background-image 0.8s ease-in-out"
+        position="absolute"
+        top={0}
+        left={0}
+      ></Box>
+
+      {/* Navegação do carrossel */}
+      <IconButton
+        aria-label="Previous Slide"
+        icon={<ArrowBackIcon boxSize={6} />}
+        onClick={handlePrevSlide}
+        position="absolute"
+        top="50%"
+        left="20px"
+        transform="translateY(-50%)"
+        zIndex={2}
+        bg="rgba(255, 255, 255, 0.2)"
+        _hover={{ bg: "rgba(255, 255, 255, 0.3)" }}
+        _active={{ bg: "rgba(255, 255, 255, 0.4)" }}
+        color="white"
+        boxShadow="lg"
+        rounded="full"
+        backdropFilter="blur(5px)"
+        transition="all 0.3s"
+      />
+      <IconButton
+        aria-label="Next Slide"
+        icon={<ArrowForwardIcon boxSize={6} />}
+        onClick={handleNextSlide}
+        position="absolute"
+        top="50%"
+        right="20px"
+        transform="translateY(-50%)"
+        zIndex={2}
+        bg="rgba(255, 255, 255, 0.2)"
+        _hover={{ bg: "rgba(255, 255, 255, 0.3)" }}
+        _active={{ bg: "rgba(255, 255, 255, 0.4)" }}
+        color="white"
+        boxShadow="lg"
+        rounded="full"
+        backdropFilter="blur(5px)"
+        transition="all 0.3s"
+      />
+
       {/* Conteúdo principal */}
       <VStack
         w={"full"}
         alignItems={"left"}
         justify={"center"}
         px={useBreakpointValue({ base: 4, md: 8 })}
-        bgGradient={"linear(to-r, blackAlpha.600, transparent)"} // Gradiente para melhor legibilidade
+        bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
         position={"relative"}
-        zIndex={1} // Garantindo que o conteúdo fique sobre o fundo
+        zIndex={1}
       >
         <Container maxW={"1200px"} px={12} py={8}>
           <Stack maxW={"2xl"} align={"flex-start"} spacing={6}>
@@ -50,7 +111,7 @@ const HeroSection = () => {
               lineHeight={1.2}
               fontSize={useBreakpointValue({ base: "3xl", md: "6xl" })}
             >
-              Bem vindos <br /> a DATA7
+              Bem-vindos <br /> à DATA7
             </Text>
 
             <Text
@@ -61,16 +122,16 @@ const HeroSection = () => {
               fontSize={useBreakpointValue({ base: "1xl", md: "2xl" })}
             >
               A Data7 oferece serviços e produtos de TI para diversas empresas.
-              Vendemos e implantamos sistemas ERP, equipamentos SAT e certificados
-              digitais. Também desenvolvemos sites de alto impacto, aplicativos e
-              bots de atendimento ao cliente. Nenhum projeto é grande ou pequeno
-              demais para nós!
+              Vendemos e implantamos sistemas ERP, equipamentos SAT e
+              certificados digitais. Também desenvolvemos sites de alto impacto,
+              aplicativos e bots de atendimento ao cliente. Nenhum projeto é
+              grande ou pequeno demais para nós!
             </Text>
             <Stack direction={"row"}>
               <Button
                 as="a"
                 href={whatsappLink}
-                target="_blank" // Abre o link em uma nova aba
+                target="_blank"
                 rel="noopener noreferrer"
                 bg={"blue.400"}
                 rounded={"15px 0px 15px"}
