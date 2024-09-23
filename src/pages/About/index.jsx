54c1d4516ae3f02d56e2b8d33d3f5717f7491fa6
Joabe Nascimento <joabefnascimento1@outlook.com>
2024-09-22 21:01:44 -0300

    foi feita a atualização do border radius do about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -148,7 +148,7 @@ export default function SplitScreen() {
                 target="_blank" // Abre o link em uma nova aba
                 rel="noopener noreferrer"
                 bg={"blue.400"}
-                rounded={"30px 30px 30px 30px"}
+                rounded={"15px 0px 15px"}
                 color={"white"}
                 _hover={{ bg: "blue.500" }}
               >
